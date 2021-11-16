562dd70272c78efbbeb195068426d4a19fc7cf55
alanfoote <afoote@uw.edu>
2021-11-15 20:58:52 -0800

    UPdate

--- script.js
+++ script.js
@@ -7,13 +7,47 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/256/{z}/{x}/{y}?access_
     accessToken: 'pk.eyJ1IjoiYWZvb3RlIiwiYSI6ImNrdm85a2NvdjZnNXMyb3Fwa3Nyb3NieGEifQ.z9saZUXAMQZ-OZ7w2iGaSg',
 }).addTo(map);
 
-var control = L.Routing.control ({
+var control = L.Routing.control({
         waypoints: [
-            L.latLng(47.246587, -122.438830),
-            L.latLng(47.258024, -122.444725),
-            L.latLng(47.318017, -122.542970)
+              null
+          //  L.latLng(47.246587, -122.438830),
+          //  L.latLng(47.258024, -122.444725),
+          //  L.latLng(47.318017, -122.542970)
 
         ],
          routeWhileDragging: true,
          router: L.Routing.mapbox('pk.eyJ1IjoiYWZvb3RlIiwiYSI6ImNrdm85a2NvdjZnNXMyb3Fwa3Nyb3NieGEifQ.z9saZUXAMQZ-OZ7w2iGaSg'),
+         units:'imperial',
+         collapsible: true,
+         autoRoute: true,
+         show: false,
+         geocoder: L.Control.Geocoder.photon(),
     }).addTo(map);
+
+    function createButton(label, container) {
+        var btn = L.DomUtil.create('button', '', container);
+        btn.setAttribute('type', 'button');
+        btn.innerHTML = label;
+        return btn;
+    }
+
+    map.on('click', function(e) {
+        var container = L.DomUtil.create('div'),
+            startBtn = createButton('Start', container),
+            destBtn = createButton('End', container);
+
+        L.popup()
+            .setContent(container)
+            .setLatLng(e.latlng)
+            .openOn(map);
+
+        L.DomEvent.on(startBtn, 'click', function() {
+          control.spliceWaypoints(0, 1, e.latlng);
+          map.closePopup();
+            });
+        L.DomEvent.on(destBtn, 'click', function() {
+          control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+          control.show();
+          map.closePopup();
+            });
+     });
